88c70bf3930cf7b209390550db62fd2f8dcd620d
Simon Conrad <112648913+SlySkillet@users.noreply.github.com>
2025-11-22 15:15:17 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { SiZcool } from "react-icons/si";
+
+function App() {
+  return (
+    <div className="grid h-screen bg-gray-900 text-amber-50">
+      <div className="flex gap-4 self-center justify-self-center">
+        <SiZcool className="text-9xl" />
+        <div className="text-md self-center font-mono font-extrabold italic">
+          <h1>Simon's </h1>
+          <h1>Super</h1>
+          <h1>Special</h1>
+          <h1>Vite</h1>
+          <h1>Special</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
