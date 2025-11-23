ff17e673c910f6506c138be1a8ac4d32fe419a37
Simon Conrad <simondbconrad@gmail.com>
2025-11-23 07:51:52 -0500

    basic button layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import { SiZcool } from "react-icons/si";
+import { TbChristmasTree } from "react-icons/tb";
+
+interface TreeButtonProps {
+  color: string;
+  tree: string;
+}
+
+const TreeButton = ({ color, tree }: TreeButtonProps) => {
+  return (
+    <div className={`w-32 rounded-lg bg-${color} m-1 p-4 text-center`}>
+      {tree}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="grid h-screen bg-gray-900 text-amber-50">
+      {/* top row */}
       <div className="flex gap-4 self-center justify-self-center">
-        <SiZcool className="text-9xl" />
+        <TbChristmasTree className="text-9xl" />
         <div className="text-md self-center font-mono font-extrabold italic">
-          <h1>Simon's </h1>
-          <h1>Super</h1>
-          <h1>Special</h1>
-          <h1>Vite</h1>
-          <h1>Special</h1>
+          <h1>Tree Counter</h1>
         </div>
+      </div>
+      {/* main */}
+
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-extrabold">F78</h1>
+        <TreeButton color="slate-400" tree="129" />
+        <TreeButton color="slate-400" tree="139" />
+        <TreeButton color="slate-400" tree="149" />
+        <TreeButton color="slate-400" tree="159" />
+        <TreeButton color="slate-400" tree="179" />
+        <TreeButton color="slate-400" tree="199" />
+        <TreeButton color="slate-400" tree="219" />
       </div>
     </div>
   );
